51732b72e8b09505692ed8f6e6ffa87a82ec2890
sherry40617 <sherry40617@gmail.com>
2021-09-19 17:15:02 +0800

    210919

--- D5-Password_Generator/main.py
+++ D5-Password_Generator/main.py
@@ -22,4 +22,41 @@ for numbers_times in range(0, nr_numbers):
     ingredients.append(random.choice(numbers))      # random.choice(LIST) == 從LIST裡面隨機選一個value
 
 random.shuffle(ingredients)     # random.shuffle(LIST) == 打亂LIST裡面的value排序
-print("".join(ingredients))     # "".join(LIST) == 用""裡面的東西分隔LIST裡的value
+password = "".join(ingredients)        # "".join(LIST) == 用""裡面的東西分隔LIST裡的value
+print(f"Your password is {password}.")
+
+
+"""
+# Solution
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password_list = []
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""               #password = str 就不用"".join()拆解LIST
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
+
+"""
